cc98c69bef01c2dacf938fd94e3c510b97de11cc
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2019-10-23 16:09:43 -0400

    docs structure

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,9 @@ sys.path.insert(0, os.path.abspath('../python/'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'RascalC'
-copyright = '2019, Oliver Philcox, Ross O\'Connell, Daniel Eisenstein, Alex Wiegand'
-author = 'Oliver Philcox, Ross O\'Connell, Daniel Eisenstein, Alex Wiegand'
+project = 'HIPSTER'
+copyright = '2019, Oliver Philcox, Daniel Eisenstein'
+author = 'Oliver Philcox, Daniel Eisenstein'
 
 # The short X.Y version
 version = ''
@@ -110,7 +110,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'RascalCdoc'
+htmlhelp_basename = 'HIPSTERdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -137,8 +137,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'RascalC.tex', 'RascalC Documentation',
-     'Oliver Philcox, Ross O\'Connell, Daniel Eisenstein, Alex Wiegand', 'manual'),
+    (master_doc, 'HIPSTER.tex', 'HIPSTER Documentation',
+     'Oliver Philcox, Daniel Eisenstein', 'manual'),
 ]
 
 
@@ -147,7 +147,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'rascalc', 'RascalC Documentation',
+    (master_doc, 'hipster', 'HIPSTER Documentation',
      [author], 1)
 ]
 
@@ -158,8 +158,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'RascalC', 'RascalC Documentation',
-     author, 'RascalC', 'One line description of project.',
+    (master_doc, 'HIPSTER', 'HIPSTER Documentation',
+     author, 'HIPSTER', 'High-k Power Spectrum Estimator',
      'Miscellaneous'),
 ]
 
